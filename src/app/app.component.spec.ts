3ad8de2ac7c1ffdb7e2cc408fa0a5a10e85d4225
lypyta <ly.samilla@duocuc.cl>
2023-10-12 22:40:11 -0300

    ruts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 
@@ -9,6 +10,7 @@ describe('AppComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [RouterTestingModule.withRoutes([])],
     }).compileComponents();
   });
 
@@ -17,5 +19,33 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+  it('should have menu labels', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.nativeElement;
+    const menuItems = app.querySelectorAll('ion-label');
+    expect(menuItems.length).toEqual(12);
+    expect(menuItems[0].textContent).toContain('Inbox');
+    expect(menuItems[1].textContent).toContain('Outbox');
+  });
+
+  it('should have urls', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.nativeElement;
+    const menuItems = app.querySelectorAll('ion-item');
+    expect(menuItems.length).toEqual(12);
+    expect(menuItems[0].getAttribute('ng-reflect-router-link')).toEqual('/folder/inbox');
+    expect(menuItems[1].getAttribute('ng-reflect-router-link')).toEqual('/folder/outbox');
+  });
+
+
+
+
+
+
+
+
+
 
 });
